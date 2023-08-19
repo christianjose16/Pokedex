65bb0bba231bd63c78fb195a1f7563c74fc7afc5
christianjose16 <josechapinx2@gmail.com>
2023-08-18 21:23:57 -0600

    Formulario
    Creación del formulario reactivo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormArray, FormBuilder, FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'demopromerica';
+export class AppComponent implements OnInit{
+  formularioPerfil: FormGroup;
+  constructor(private fb: FormBuilder) {
+    this.formularioPerfil = this.fb.group({
+      'nombre': new FormControl('', Validators.required),
+      'pasatiempo': new FormControl('',Validators.required),
+      'cumple': new FormControl('',Validators.required),
+      'documento': new FormControl('',Validators.required)
+    });
+  }
+  ngOnInit() {
+
+  }
 }
